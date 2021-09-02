b5b644f3305dbd77de13d13aad0f202d8a091a77
Ryan <ryan.mossor@gmail.com>
2021-09-02 11:25:07 -0500

    Created controller to GET custom login page

--- src/main/java/com/liftoff/libraryapp/registration/LoginController.java
+++ src/main/java/com/liftoff/libraryapp/registration/LoginController.java
@@ -3,11 +3,6 @@ package com.liftoff.libraryapp.registration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 @Controller
 public class LoginController {
@@ -16,14 +11,6 @@ public class LoginController {
     public String displayLoginForm(Model model) {
         model.addAttribute("title", "Log In");
         return "login";
-    }
-
-    @PostMapping("/login")
-    public String processLoginForm(@RequestParam String username, @RequestParam String password) {
-
-        System.out.println("Post test");
-        return "redirect:";
-
     }
 
 }
