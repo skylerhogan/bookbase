905235ff53f1364978d29ee991db7e8e769cd72b
skylerhogan <skyler.hogan@gmail.com>
2021-09-06 14:46:04 -0500

    added delete.js to toggle disabled delete button

--- src/main/resources/static/js/delete.js
+++ src/main/resources/static/js/delete.js
@@ -1,28 +1,38 @@
 const books = document.querySelectorAll("img");
 const deleteButton = document.querySelector("#deleteButton");
-const bookIds = document.getElementsByName("bookIds");
+deleteButton.disabled = true;
 
+let selected = [];
 
 for(let i=0; i < books.length; i++) {
+
     books[i].addEventListener('click', function (e) {
+
     if(books[i].style.opacity == "50%") {
         books[i].style.opacity = "100%";
         books[i].style.height = "200px";
         books[i].style.width = "133px"
         books[i].style.marginTop = "0rem";
+        selected.splice(books[i],1);
         } else {
         books[i].style.opacity = "50%";
         books[i].style.height = "180px";
         books[i].style.width = "120px";
         books[i].style.alignItems = "center";
         books[i].style.marginTop = ".3rem";
+        selected.push(books[i]);
+        deleteButton.disabled = false;
+        };
 
-        }
+    if (selected.length == 0) {
+        deleteButton.disabled = true;
+        };
     })
+
 };
 
 const deleteBooks = () => {
     document.getElementById('deleteForm').submit();
-}
+};
 
 
