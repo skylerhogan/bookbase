cb40907a950c0c04c786df2d1f56d56d498f39c5
Skyler Hogan <skylerhogan@skylers-imac.attlocal.net>
2021-09-23 10:45:13 -0500

    updated Google Books API key

--- src/main/resources/static/script/search/results.js
+++ src/main/resources/static/script/search/results.js
@@ -7,7 +7,7 @@ let promises = [];
 let totalResults = 0;
 
 const API_ENDPOINT = "https://www.googleapis.com";
-const API_KEY = "AIzaSyDk87M-Tr5KQMeR2ZlCIjQ2nEsqiAo-uMg";
+const API_KEY = "AIzaSyCqhSRjhDB0_STP1_G8EAsgLI1IbJHhdno;
 //const API_KEY = "AIzaSyDCaZHgbjWrCjgUI1LBgw3e3-U_P6ua4kI"
 
 let results = document.getElementById('results');
